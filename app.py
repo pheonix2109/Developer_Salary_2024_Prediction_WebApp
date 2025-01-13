e614a6e3c9a5943e22c1ddb16c0b4e355dd9d18d
pheonix2109 <pallavijagre123@gmail.com>
2025-01-13 17:04:24 +0530

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pred_page import show_predict_page
+from predict_page import show_predict_page
 from explore_page import show_explore_page
 
 page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore"))
